f339e10ccd9bb78e90848ba18366612bd83751ac
NOMAN1802 <mustakimnoman82@gmail.com>
2024-07-09 02:22:58 +0600

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+
+async function main() {
+
+    try {
+
+        await mongoose.connect(config.bd_url as string);
+        app.listen(config.port, () => {
+            console.log(`ClickCraft listening on port ${config.port}`)
+          });
+        
+    } catch (error) {
+        console.log(error)
+    }
+   
+  
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  }
+
+  main();
